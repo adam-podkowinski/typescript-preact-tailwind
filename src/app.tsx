0e35e0593ddc5b9f377f428a242da7e915ff7d90
Adam Podkowinski <adampodkdev@gmail.com>
2022-12-18 15:32:34 +0100

    Add readme

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
-import classes from "./app.module.scss";
-
 export const App = () => (
-  <div class={classes.style}>VITE + PREACT + TYPESCRIPT</div>
+  <div
+    class={
+      "grid h-screen place-items-center bg-slate-900 px-3 text-5xl font-semibold text-blue-400"
+    }
+  >
+    <p class={"hover:text-blue-300"}>VITE + PREACT + TYPESCRIPT + TAILWIND</p>
+  </div>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
 });
